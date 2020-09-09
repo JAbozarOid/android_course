0dec2b460de22df518143605693a7ba0a044587f
JAbozarOid <araghibdoust@hotmail.com>
2020-09-09 17:07:13 +0430

    variables

---
+++ VariablesClass.java
@@ -0,0 +1,25 @@
+public class VariablesClass {
+    public static void main(String[] args) {
+        
+        System.out.println("Abozar");
+
+        int number = 1;
+        System.out.println(number);
+
+        double decimalNumber = 1.5;
+        System.out.println(decimalNumber);
+
+        char character = 'A';
+        System.out.println(character);
+
+        String text = "Abozar Raghibdoust";
+        System.out.println(text);
+
+        boolean bln = true;
+        System.out.println(bln);
+
+
+
+
+    }
+}
