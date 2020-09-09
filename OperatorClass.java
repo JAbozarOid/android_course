8a40f970d9761e278c9b35b4a4b4e132f6aa3d8c
JAbozarOid <araghibdoust@hotmail.com>
2020-09-09 17:43:42 +0430

    arithmatic_operators

---
+++ OperatorClass.java
@@ -0,0 +1,27 @@
+public class OperatorClass {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 14;
+        int sum = a + b;
+        System.out.println(sum);
+
+        int sub = a - b;
+        System.out.println(sub);
+
+        int multiple = a * b;
+        System.out.println(multiple);
+
+        // OUTPUT -> KHAREJGHESMAT
+        float division = a / b;
+        System.out.format("The value of " + "the integer a is " + "%d, while the value of the "
+                + "integer variable b is %d, " + "and the KHAREJGHESMAT is %f", a, b, division);
+
+        System.out.println("\n");
+
+        // OUTPUT -> BAGHIMANDE = REMAIN
+        float divide = a % b;
+        System.out.format("The value of " + "the integer a is " + "%d, while the value of the "
+                + "integer variable b is %d, " + "and the BAGHIMANDE is %f", a, b, divide);
+
+    }
+}
