d90b5a4311204db3c902ebd635f5bd554c6df9b4
JAbozarOid <araghibdoust@hotmail.com>
2020-09-09 18:05:26 +0430

    define_operators

--- OperatorClass.java
+++ OperatorClass.java
@@ -1,5 +1,7 @@
 public class OperatorClass {
     public static void main(String[] args) {
+        
+        // start of define operators in java
         int a = 20;
         int b = 14;
         int sum = a + b;
@@ -22,6 +24,8 @@ public class OperatorClass {
         float divide = a % b;
         System.out.format("The value of " + "the integer a is " + "%d, while the value of the "
                 + "integer variable b is %d, " + "and the BAGHIMANDE is %f", a, b, divide);
+        
+        // end of define operators in java
 
     }
 }
