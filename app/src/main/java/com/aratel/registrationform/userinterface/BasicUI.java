039dddd038ae0187e3ebbc445bb2cb019b6e94f6
JAbozarOid <araghibdoust@hotmail.com>
2020-10-18 13:32:29 +0330

    basic_ui

---
+++ app/src/main/java/com/aratel/registrationform/userinterface/BasicUI.java
@@ -0,0 +1,16 @@
+package com.aratel.registrationform.userinterface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aratel.registrationform.R;
+
+public class BasicUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_u_i);
+    }
+}
